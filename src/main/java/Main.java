c0a306b4689ab57de7a50a8fedb8a8df550c5122
issam <issambenslimane9999@hotmail.com>
2026-02-03 22:39:39 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println("$ " + args[0]);
         System.out.print(args[0] + ": command not found");
     }
 }
